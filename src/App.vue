051a91da722da9b54ae5575bd867f0c4f599ad68
yagobelo <yago.belo00@gmail.com>
2024-05-21 14:41:40 -0300

    fix: ajuste no App.vue e index.html

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@ import { RouterLink, RouterView } from "vue-router";
 
 <template>
   <header>
+    <h1 class="titleHeader">HOTEL XY</h1>
     <nav>
       <RouterLink class="textHeader" to="/hospedes">HOSPEDES</RouterLink>
+      <img src="../public/logo.png" alt="LogoHotel" />
       <RouterLink class="textHeader" to="/reservas">RESERVAS</RouterLink>
     </nav>
   </header>
@@ -16,24 +18,36 @@ import { RouterLink, RouterView } from "vue-router";
 header {
   background: #00b4d8;
   border-radius: 10px;
-  height: 50px;
+  height: 75px;
   min-width: 240px;
+}
+.titleHeader {
+  font-size: 20px;
+  text-align: center;
+  margin: 0;
+  padding: 5px 0 0 0;
 }
 nav {
   display: flex;
   justify-content: center;
-  height: 100%;
   align-items: center;
 }
 .textHeader {
+  background: #009cbb;
   color: black;
   text-decoration: none;
   font-family: Arial, Helvetica, sans-serif;
   font-size: 20px;
   font-weight: bold;
   margin: 10px;
+  padding: 0 10px;
+  border-radius: 10px;
 }
 .textHeader:hover {
   color: white;
+}
+img {
+  width: 40px;
+  height: 40px;
 }
 </style>
